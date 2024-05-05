12335a0eb866e94283eb1aaa2f2a0c5e6c947582
Mohit Pal Singh <mohit.pal.singh@outlook.com>
2024-05-06 03:18:12 +0530

    fixed encoding

--- p2p/encoding.go
+++ p2p/encoding.go
@@ -18,6 +18,17 @@ func (dec GOBDecoder) Decode(r io.Reader, msg *RPC) error {
 type DefaultDecoder struct{}
 
 func (dec DefaultDecoder) Decode(r io.Reader, msg *RPC) error {
+	peekbuf := make([]byte, 1)
+	if _, err := r.Read(peekbuf); err != nil {
+		return err
+	}
+	// in case of stream, no decoding will take place, will be handled in logic
+	stream := peekbuf[0] == IncomingStream
+	if stream {
+		msg.Stream = true
+		return nil
+	}
+
 	buf := make([]byte, 1028)
 	n, err := r.Read(buf)
 	if err != nil {
--- p2p/message.go
+++ p2p/message.go
@@ -2,7 +2,13 @@ package p2p
 
 import "net"
 
+const (
+	IncomingMessage = 0x1
+	IncomingStream  = 0x2
+)
+
 type RPC struct {
 	From    net.Addr
 	Payload []byte
+	Stream  bool
 }
